81d087a27c5236101372d12968068c4bf30f9699
lslvv34 <lslvv34@users.noreply.github.com>
2025-05-29 07:31:06 +0000

    auto deploy lslvv34/action-hexo@a52b90c5d5a86ee51f7433e64dbb2330e5056551

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/29/hello-world/","2025/05/29/测试一下/","2025/05/29/第一篇文章/"];function toRandomPost(){
+var posts=["2025/05/29/hello-world/","2025/05/29/第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
